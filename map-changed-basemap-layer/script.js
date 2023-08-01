e733b043caaa9c09fa80efeec690db1aed7f7cbb
oskarziembrowicz <rakson2002@gmail.com>
2023-08-01 12:23:26 +0200

    Toggling between two basemaps

---
+++ map-changed-basemap-layer/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+require([
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/widgets/BasemapToggle",
+  "esri/widgets/BasemapGallery",
+], function (esriConfig, Map, MapView, BasemapToggle, BasemapGallery) {
+  esriConfig.apiKey =
+    "AAPK1048c04e0d1a4046b15f7383bf8d72e555Dqjx7S4pgbyhPx5Tug3bRK3MwLF4eHcK3Y3n1c9uao-mV4gfHuKQtSKLXMuOVn";
+
+  const map = new Map({
+    basemap: "arcgis-topographic",
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    // Santa Monica Mountains in California:
+    center: [-118.805, 34.027],
+    zoom: 13,
+  });
+
+  const basemapToggle = new BasemapToggle({
+    view: view,
+    nextBasemap: "arcgis-imagery",
+  });
+
+  view.ui.add(basemapToggle, "bottom-right");
+});
