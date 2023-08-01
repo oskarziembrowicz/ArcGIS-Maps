61c8f9ec9ea7d4774fa76927a8289acf223f05f7
oskarziembrowicz <rakson2002@gmail.com>
2023-08-01 14:30:52 +0200

    Displaying pop-ups for points

---
+++ displaying-a-pop-up/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+require([
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+], function (esriConfig, Map, MapView, FeatureLayer) {
+  esriConfig.apiKey =
+    "AAPK1048c04e0d1a4046b15f7383bf8d72e555Dqjx7S4pgbyhPx5Tug3bRK3MwLF4eHcK3Y3n1c9uao-mV4gfHuKQtSKLXMuOVn";
+
+  const map = new Map({
+    basemap: "arcgis-topographic",
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    // Santa Monica Mountains in California:
+    center: [-118.805, 34.027],
+    zoom: 13,
+  });
+
+  // Trailheads pop-up
+  const popupTrailheads = {
+    title: "Trailhead",
+    content:
+      "<b>Trail:</b> {TRL_NAME}<br><b>City:</b> {CITY_JUR}<br><b>Cross Street:</b> {X_STREET}<br><b>Parking:</b> {PARKING}<br><b>Elevation:</b> {ELEV_FT} m",
+  };
+
+  const trailheads = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0",
+    outFields: ["TRL_NAME", "CITY_JUR", "X_STREET", "PARKING", "ELEV_FT"],
+    popupTemplate: popupTrailheads,
+  });
+
+  map.add(trailheads);
+});
