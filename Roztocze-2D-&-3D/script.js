947d081a67e86b9906d4993a5825c05555cd1142
oskarziembrowicz <rakson2002@gmail.com>
2023-08-08 15:32:23 +0200

    Adding two maps: 2D & 3D

--- Roztocze-2D-&-3D/script.js
+++ Roztocze-2D-&-3D/script.js
@@ -1,22 +1,35 @@
 "use strict";
 
-require(["esri/config", "esri/Map", "esri/views/MapView"], function (
-  esriConfig,
-  Map,
-  MapView
-) {
+require([
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/views/SceneView",
+], function (esriConfig, Map, MapView, SceneView) {
   esriConfig.apiKey =
     "AAPK1048c04e0d1a4046b15f7383bf8d72e555Dqjx7S4pgbyhPx5Tug3bRK3MwLF4eHcK3Y3n1c9uao-mV4gfHuKQtSKLXMuOVn";
 
-  // const map = new Map({
-  //   basemap: "arcgis-topographic",
-  // });
+  const map = new Map({
+    basemap: "arcgis-topographic",
+  });
 
-  // const view = new MapView({
-  //   container: "viewDiv",
-  //   map: map,
-  //   // Santa Monica Mountains in California:
-  //   center: [-118.805, 34.027],
-  //   zoom: 13,
-  // });
+  const viewOptions = {
+    map: map,
+    center: [23.013015, 50.611453],
+    zoom: 13,
+  };
+
+  const view2d = new MapView(viewOptions);
+  view2d.container = "viewDiv2d";
+
+  const view3d = new SceneView(viewOptions);
+  view3d.container = "viewDiv3d";
+  // view3d.camera = {
+  //   posiition: {
+  //     x: viewOptions.center[0],
+  //     y: viewOptions.center[1],
+  //     z: 2000,
+  //   },
+  //   tilt: 75,
+  // };
 });
