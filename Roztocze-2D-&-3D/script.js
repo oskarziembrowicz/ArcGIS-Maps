34dacf471b6e391d9ad7eecd234099077ac9ca49
oskarziembrowicz <rakson2002@gmail.com>
2023-08-08 15:45:16 +0200

    Synchronizing views

--- Roztocze-2D-&-3D/script.js
+++ Roztocze-2D-&-3D/script.js
@@ -24,12 +24,28 @@ require([
 
   const view3d = new SceneView(viewOptions);
   view3d.container = "viewDiv3d";
-  // view3d.camera = {
-  //   posiition: {
-  //     x: viewOptions.center[0],
-  //     y: viewOptions.center[1],
-  //     z: 2000,
-  //   },
-  //   tilt: 75,
-  // };
+
+  const views = [view2d, view3d];
+  let active; // Currently 'active' view
+
+  function sync(source) {
+    if (!active || !active.viewpoint || active !== source) {
+      return;
+    }
+
+    for (const view of views) {
+      if (view !== active) {
+        view.viewpoint = active.viewpoint;
+      }
+    }
+  }
+
+  for (const view of views) {
+    view.watch(["intereacting", "animation"], () => {
+      active = view;
+      sync(active);
+    });
+
+    view.watch("viewpoint", () => sync(view));
+  }
 });
