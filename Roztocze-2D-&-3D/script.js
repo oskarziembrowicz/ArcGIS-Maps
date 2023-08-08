13f4925adc9c9e437aa25dfb87f589dee116d421
oskarziembrowicz <rakson2002@gmail.com>
2023-08-08 15:22:22 +0200

    Starting new application with two maps side-by-side (2D and 3D)

---
+++ Roztocze-2D-&-3D/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+require(["esri/config", "esri/Map", "esri/views/MapView"], function (
+  esriConfig,
+  Map,
+  MapView
+) {
+  esriConfig.apiKey =
+    "AAPK1048c04e0d1a4046b15f7383bf8d72e555Dqjx7S4pgbyhPx5Tug3bRK3MwLF4eHcK3Y3n1c9uao-mV4gfHuKQtSKLXMuOVn";
+
+  // const map = new Map({
+  //   basemap: "arcgis-topographic",
+  // });
+
+  // const view = new MapView({
+  //   container: "viewDiv",
+  //   map: map,
+  //   // Santa Monica Mountains in California:
+  //   center: [-118.805, 34.027],
+  //   zoom: 13,
+  // });
+});
