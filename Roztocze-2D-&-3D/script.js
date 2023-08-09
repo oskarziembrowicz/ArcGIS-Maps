9f52f835dc63f3b3c8451aca1eab1e9a08535c96
oskarziembrowicz <rakson2002@gmail.com>
2023-08-09 14:05:41 +0200

    Adding a few simple widgets: search, scale bar, home, locate

--- Roztocze-2D-&-3D/script.js
+++ Roztocze-2D-&-3D/script.js
@@ -9,6 +9,12 @@ require([
   "esri/layers/VectorTileLayer",
   "esri/layers/BaseElevationLayer",
   "esri/layers/ElevationLayer",
+  "esri/widgets/Search",
+  "esri/widgets/Locate",
+  "esri/widgets/ScaleBar",
+  "esri/widgets/Home",
+  "esri/widgets/Daylight",
+  "esri/widgets/Expand",
 ], function (
   esriConfig,
   Map,
@@ -17,7 +23,13 @@ require([
   FeatureLayer,
   VectorTileLayer,
   BaseElevationLayer,
-  ElevationLayer
+  ElevationLayer,
+  Search,
+  Locate,
+  ScaleBar,
+  Home,
+  Daylight,
+  Expand
 ) {
   esriConfig.apiKey =
     "AAPK1048c04e0d1a4046b15f7383bf8d72e555Dqjx7S4pgbyhPx5Tug3bRK3MwLF4eHcK3Y3n1c9uao-mV4gfHuKQtSKLXMuOVn";
@@ -98,6 +110,35 @@ require([
     },
     tilt: 75,
   };
+
+  // Widgets:
+
+  const search = new Search({
+    view: view2d,
+  });
+  const expandSearch = new Expand({
+    view: view2d,
+    content: search,
+    expandIcon: "search",
+  });
+  view2d.ui.add(expandSearch, "top-right");
+
+  const locate = new Locate({
+    view: view2d,
+  });
+  view2d.ui.add(locate, "bottom-right");
+
+  const scaleBar = new ScaleBar({
+    view: view2d,
+  });
+  view2d.ui.add(scaleBar, "bottom-left");
+
+  const home = new Home({
+    view: view2d,
+  });
+  view2d.ui.add(home, "top-left");
+
+  // Synchronize views:
 
   const views = [view2d, view3d];
   let active; // Currently 'active' view
