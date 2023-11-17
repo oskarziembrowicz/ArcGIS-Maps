cdaf697608fbd482f6fe7daae61036c13a026fda
oskarziembrowicz <rakson2002@gmail.com>
2023-11-17 18:13:25 +0100

    Displaying a map

--- YT-Tutorials/arcgis-start/src/App.jsx
+++ YT-Tutorials/arcgis-start/src/App.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
 import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
 
 function App() {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const view = new MapView({
-      container: mapRef.current,
+    if (!mapRef?.current) return;
+
+    const map = new Map({
+      basemap: "osm",
     });
+    const view = new MapView({
+      map: map,
+      container: mapRef.current,
+      center: [55, 25],
+      zoom: 13,
+    });
+
+    return () => view && view.destroy();
   }, []);
 
   return <div className="viewDiv" ref={mapRef}></div>;
