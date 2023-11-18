26f9f7446041288046fb466459de91d09e10749a
oskarziembrowicz <rakson2002@gmail.com>
2023-11-18 13:27:10 +0100

    Adding graphic point

--- YT-Tutorials/arcgis-start/src/ArcGIS-Components/ArcMapView/ArcMapView.jsx
+++ YT-Tutorials/arcgis-start/src/ArcGIS-Components/ArcMapView/ArcMapView.jsx
@@ -1,4 +1,10 @@
 import { useEffect, useRef } from "react";
+
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+
 import "./ArcMapView.css";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
@@ -22,6 +28,23 @@ export const ArcMapView = () => {
     if (!mapRef?.current) return;
 
     const view = createMapView(mapRef.current);
+
+    const graphicsLayer = new GraphicsLayer();
+    view.map.add(graphicsLayer);
+
+    const point = new Point({
+      longitude: 55,
+      latitude: 25,
+    });
+    const simpleMarkerSymbol = new SimpleMarkerSymbol({
+      color: "red",
+    });
+    const graphicPoint = new Graphic({
+      geometry: point,
+      symbol: simpleMarkerSymbol,
+    });
+
+    graphicsLayer.add(graphicPoint);
 
     return () => view && view.destroy();
   }, []);
